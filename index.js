a60af4e9be3409f405f76e3b0b46db59190baa39
Izalpha <visualstudio530@naver.com>
2021-01-19 02:53:17 +0900

    [#2.2] DOM

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-const calculator = {
-    add : function(a,b) {
-        return a + b;
-    },
-    subtract : function(a,b) {
-        return a - b;
-    },
-    multy: function(a,b) {
-        return a * b;
-    },
-    divide: function(a,b) {
-        return a / b;
-    }
-}
-
-console.log(calculator.add(5,5));
-
-console.log(calculator.subtract(5,5));
-
-console.log(calculator.multy(5,5));
-
-console.log(calculator.divide(5,5));
+const title = document.getElementById("title");
+title.innerHTML = "Hi, JS!";
