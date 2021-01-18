5c676617375d6c939d17e9ff9fc6a170bf087133
Izalpha <visualstudio530@naver.com>
2021-01-18 17:39:06 +0900

    JS #1

---
+++ index.js
@@ -0,0 +1,13 @@
+const jihyeokInfo = {
+    name: "Jihyeok",
+    age: 22,
+    gender: "Male",
+    favFood: ["스시","돈카츠","사케동","규카츠"],
+    girlfrined : {
+    name : "NONE",
+    age : "NO" ,
+    gender : "NOPE"
+    }   
+}
+
+console.log(jihyeokInfo);
