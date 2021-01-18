525f8d450795f97f91c758dde3aff546fb0d0b1c
Izalpha <visualstudio530@naver.com>
2021-01-19 02:28:05 +0900

    [#2.1]calculator

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const jihyeokInfo = {
-    name: "Jihyeok",
-    age: 22,
-    gender: "Male",
-    favFood: ["스시","돈카츠","사케동","규카츠"],
-    girlfrined : {
-    name : "NONE",
-    age : "NO" ,
-    gender : "NOPE"
-    }   
+const calculator = {
+    add : function(a,b) {
+        return a + b;
+    },
+    subtract : function(a,b) {
+        return a - b;
+    },
+    multy: function(a,b) {
+        return a * b;
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
 }
 
-console.log(jihyeokInfo);
+console.log(calculator.add(5,5));
+
+console.log(calculator.subtract(5,5));
+
+console.log(calculator.multy(5,5));
+
+console.log(calculator.divide(5,5));
